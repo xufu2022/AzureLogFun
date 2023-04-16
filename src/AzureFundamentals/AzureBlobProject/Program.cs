56b658acd68c7f8d2c5954da734bdf2b10d29f8b
xufu2022 <94775437+xufu2022@users.noreply.github.com>
2023-04-16 18:30:06 +0100

    blog contianer-end

--- src/AzureFundamentals/AzureBlobProject/Program.cs
+++ src/AzureFundamentals/AzureBlobProject/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddSingleton(u => new BlobServiceClient(
     builder.Configuration.GetValue<string>("BlobConnection")
 ));
 builder.Services.AddSingleton<IContainerService, ContainerService>();
+builder.Services.AddSingleton<IBlobService, BlobService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
